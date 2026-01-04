75ba635f3e753815f42aec04c8e71efcfedab1e7
Artur PC <al211205@yandex.ru>
2026-01-04 23:00:15 +0300

    Класс конфигурации секретов БД

--- Backend/classes.py
+++ Backend/classes.py
@@ -2,6 +2,15 @@ from dataclasses import dataclass
 
 
 @dataclass()
+class DbConfig:
+    admin: str
+    password: str
+    host: str
+    port: int
+    db_name: str
+
+
+@dataclass()
 class Appeal:
     user_from_id: int
     last_msg: str

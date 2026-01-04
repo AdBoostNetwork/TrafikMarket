6fa3f72a8f407da80e847e88f9b8010a7ebbf5e0
ArturErnandes <al211205@yandex.ru>
2026-01-04 19:30:26 +0300

    Макет бд Appeal

---
+++ Backend/config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class DbConfig:
+    admin: str
+    password: str
+    host: str
+    port: int
+    db_name: str
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DB_CONFIG = DbConfig(
+    admin=os.getenv("DB_ADMIN"),
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    db_name=os.getenv("DB_NAME"),
+)

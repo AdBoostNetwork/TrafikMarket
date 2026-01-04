dd70f17e157be243018475e15231df6aaf3bb46a
ArturErnandes <al211205@yandex.ru>
2026-01-05 00:01:42 +0300

    Функции msg_to_support, msg_from_support

--- Backend/bot_func.py
+++ Backend/bot_func.py
@@ -1,5 +1,33 @@
-def msg_to_support(user_id, text):
-    """получает appeal() по user_id, если нет, то создает, добавляет text к appeal.chat, ставит appeal.status=response_waiting
-    :param user_id:
-    :param text:
-    """
+from database import get_appeal_buy_id, change_appeal, create_appeal
+from classes import Appeal
+
+async def msg_to_support(user_id, text):
+    appeal = await get_appeal_buy_id(user_id)
+
+    if appeal == "appeal_not_found":
+        new_appeal = Appeal(
+            user_from_id=user_id,
+            last_msg=text,
+            chat=f"Сообщение пользователя: {text}",
+            status="response_waiting",
+        )
+        return await create_appeal(new_appeal)
+
+    return await change_appeal(
+        user_from_id=user_id,
+        status="response_waiting",
+        last_msg=text,
+        chat_append=f"Сообщение пользователя: {text}",
+    )
+
+
+
+async def msg_from_support(user_id, text):
+
+    await change_appeal(
+        user_from_id=user_id,
+        status="closed",
+        last_msg=text,
+        chat_append=f"Сообщение админа: {text}")
+
+    return

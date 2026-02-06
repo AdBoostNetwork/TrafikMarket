b8b027083f681faaf02a57b72970bd53e91ea0c0
ArturErnandes <al211205@yandex.ru>
2026-02-07 01:22:02 +0300

    Добавлено логгирование и обработка ошибок в функции get_profile

--- backend/app_backend/pages/profile.py
+++ backend/app_backend/pages/profile.py
@@ -1,21 +1,34 @@
 from fastapi import APIRouter
+import html
 
 from ..app_classes import MyProfile
 from ..database import get_profile_info_db
+from ..logger import get_logger
+
+
+logger = get_logger(__name__)
 
 profile_page = APIRouter()
 
 
 @profile_page.get("/my_profile", tags=["Профиль"], summary="Получение данных профиля")
 def get_profile(user_id: int):
-    data = get_profile_info_db(user_id)
+    try:
+        data = get_profile_info_db(user_id)
 
-    profile_info = MyProfile(
-        name=data["name"],
-        deals_count=data["deals_count"],
-        success_deals=data["success_deals"],
-        balance=data["balance"],
-        deps_list=data["deps_list"],
-    )
+        profile_info = MyProfile(
+            name=data["name"],
+            deals_count=data["deals_count"],
+            success_deals=data["success_deals"],
+            balance=data["balance"],
+            deps_list=data["deps_list"],
+        )
 
-    return profile_info
+        logger.info("Данные профиля пользователя %s успешно получены", user_id)
+        return profile_info
+
+    except Exception as e:
+        short_error = html.escape(str(e))
+
+        logger.error("Ошибка при получении данных профиля | Пользователь: %s | Ошибка: %s", user_id, short_error)
+        return {"error": short_error}

971d3b24610fc14bcf2ce3aa51d87005438c1d37
ArturErnandes <al211205@yandex.ru>
2026-02-07 00:18:35 +0300

    Загрузка конфига БД

--- backend/app_backend/app_config.py
+++ backend/app_backend/app_config.py
@@ -1,2 +1,25 @@
 # Конфигуратор app
 
+import json
+from pathlib import Path
+
+from app_classes import AppDbConfig
+
+
+DB_SECRETS_PATH = Path(__file__).resolve().parents[2] / "data" / "db_secrets.json"
+
+
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+db_data = load_json(DB_SECRETS_PATH)
+
+DbConfig = AppDbConfig(
+    admin = db_data["admin"],
+    password = db_data["password"],
+    host = db_data["host"],
+    port = db_data["port"],
+    db_name = db_data["db_name"]
+)

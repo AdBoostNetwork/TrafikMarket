cbe2b43649dfc6275b4dfe8fdfa5c7153f4a37fc
ArturErnandes <al211205@yandex.ru>
2026-02-07 01:12:04 +0300

    Класс Transaction и MyProfile

--- backend/app_backend/app_classes.py
+++ backend/app_backend/app_classes.py
@@ -17,3 +17,21 @@ class FastApiConfig:
     """Класс конфигурации хоста и порта основного приложения"""
     host: str
     port: int
+
+
+@dataclass(frozen=True)
+class Transaction:
+    """История транзакций пользователя (тип, сумма, дата совершения)"""
+    trn_type: str
+    trn_summ: str
+    trn_date: str
+
+
+@dataclass(frozen=True)
+class MyProfile:
+    """Класс с данными профиля"""
+    name: str
+    deals_count: int
+    success_deals: int
+    balance: float
+    deps_list: list[Transaction]

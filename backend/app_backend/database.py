79a729e7f3fc8d3cdbad73e132fa2e954b5ae042
ArturErnandes <al211205@yandex.ru>
2026-02-07 01:12:13 +0300

    Engine

--- backend/app_backend/database.py
+++ backend/app_backend/database.py
@@ -8,3 +8,7 @@ from app_config import DbConfig
 engine = create_async_engine(
     f'postgresql+asyncpg://{DbConfig.admin}:{DbConfig.password}@{DbConfig.host}:{DbConfig.port}/{DbConfig.database}',
 )
+
+
+def get_profile_info_db(user_id: int):
+    ...

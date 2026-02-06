91da036bf84245e6da6004f3825650e4daf2b414
Artur PC <al211205@yandex.ru>
2026-02-06 21:35:23 +0300

    Класс конфигурации БД и FastAPI

--- backend/app_backend/app_classes.py
+++ backend/app_backend/app_classes.py
@@ -1,1 +1,19 @@
 #Классы app
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class AppDbConfig:
+    """Класс конфигурации данных БД основного приложения"""
+    admin: str
+    password: str
+    host: str
+    port: int
+    db_name: str
+
+
+@dataclass(frozen=True)
+class FastApiConfig:
+    """Класс конфигурации хоста и порта основного приложения"""
+    host: str
+    port: int

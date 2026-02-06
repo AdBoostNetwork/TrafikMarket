215fb48381080045749e299221f675c36f1830ea
ArturErnandes <al211205@yandex.ru>
2026-02-07 00:42:04 +0300

    Engine

--- backend/app_backend/database.py
+++ backend/app_backend/database.py
@@ -1,1 +1,10 @@
 #Файл с функциями БД
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from app_config import DbConfig
+
+
+engine = create_async_engine(
+    f'postgresql+asyncpg://{DbConfig.admin}:{DbConfig.password}@{DbConfig.host}:{DbConfig.port}/{DbConfig.database}',
+)

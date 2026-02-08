f45fb3bfe08b3ee3389a68eba432284029dd82f4
ArturErnandes <al211205@yandex.ru>
2026-02-08 21:22:07 +0300

    Функция запуска

--- backend/app_backend/app.py
+++ backend/app_backend/app.py
@@ -1,12 +1,27 @@
 #Точка входа в сайт. Запуск fastapi
 
 from fastapi import FastAPI
+import uvicorn
+import html
 
-from pages.profile import profile_page
-from logger import get_logger
+from .pages.profile import profile_page
+from .app_config import AppConfig
+from .logger import get_logger
 
 
 logger = get_logger(__name__)
+
+
+def app_run():
+    try:
+        logger.info("Запуск FastAPI app")
+        uvicorn.run(app, host=AppConfig.host, port=AppConfig.port)
+
+    except Exception as e:
+        short_error = html.escape(str(e))
+        logger.error("Ошибка при запуске FastAPI app: %s", short_error)
+
+
 
 app = FastAPI()
 

8dfffa1be77dd1bce219b813362971e35852e0af
ArturErnandes <al211205@yandex.ru>
2026-02-08 15:44:53 +0300

    Импорт секретов приложения роутера

--- backend/app_backend/app_config.py
+++ backend/app_backend/app_config.py
@@ -3,10 +3,11 @@
 import json
 from pathlib import Path
 
-from app_classes import AppDbConfig
+from app_classes import AppDbConfig, FastApiConfig
 
 
 DB_SECRETS_PATH = Path(__file__).resolve().parents[2] / "data" / "db_secrets.json"
+APP_SECRETS_PATH = Path(__file__).resolve().parents[2] / "data" / "app_secrets.json"
 
 
 def load_json(file_path):
@@ -15,6 +16,7 @@ def load_json(file_path):
 
 
 db_data = load_json(DB_SECRETS_PATH)
+app_data = load_json(APP_SECRETS_PATH)
 
 DbConfig = AppDbConfig(
     admin = db_data["admin"],
@@ -22,4 +24,9 @@ DbConfig = AppDbConfig(
     host = db_data["host"],
     port = db_data["port"],
     db_name = db_data["db_name"]
+)
+
+AppConfig = FastApiConfig(
+    host = app_data["host"],
+    port = app_data["port"]
 )

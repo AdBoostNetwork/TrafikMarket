4d62140074f0efb692ca80c5444099345bca0919
ArturErnandes <al211205@yandex.ru>
2026-02-08 23:04:17 +0300

    Функция получения данных для страницы пользователя

--- backend/app_backend/pages/profile.py
+++ backend/app_backend/pages/profile.py
@@ -12,16 +12,16 @@ profile_page = APIRouter()
 
 
 @profile_page.get("/my_profile", tags=["Профиль"], summary="Получение данных профиля")
-def get_profile(user_id: int):
+async def get_profile(user_id: int):
     try:
-        data = get_profile_info_db(user_id)
+        data = await get_profile_info_db(user_id)
 
         profile_info = MyProfile(
             name=data["name"],
             deals_count=data["deals_count"],
-            success_deals=data["success_deals"],
-            balance=data["balance"],
-            deps_list=data["deps_list"],
+            success_deals=data["success_count"],
+            balance=data["current_balance"],
+       #     deps_list=data["deps_list"],
         )
 
         logger.info("Данные профиля пользователя %s успешно получены", user_id)

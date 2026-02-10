f299f4405818cd89d7353e01b9221a25c60da572
ArturErnandes <al211205@yandex.ru>
2026-02-10 14:03:36 +0300

    Вынос файлов в папку app_database

--- backend/app_backend/app_database/profile_db.py
+++ backend/app_backend/app_database/profile_db.py
@@ -3,8 +3,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
 
-from .app_config import DbConfig
-from .logger import get_logger
+from ..app_config import DbConfig
+from ..logger import get_logger
 
 
 logger = get_logger(__name__)

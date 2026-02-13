b03657254c3a91f2011be764324027a4a0d9eb76
ArturErnandes <al211205@yandex.ru>
2026-02-13 19:12:20 +0300

    Ручка получения объявления

--- backend/app_backend/pages/opened_announ.py
+++ backend/app_backend/pages/opened_announ.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 import html
 
-from ..app_classes import AnnounPageSchema
+from ..app_database.opened_announ_db import get_announ_page_db
 from ..logger import get_logger
 
 
@@ -12,4 +12,13 @@ opened_announ_page = APIRouter()
 
 @opened_announ_page.get("/get_announ_info", tags=["Страница объявления"], summary="Получение данных объявления")
 async def get_announ_info(announ_id: int):
-    ...
+    logger.info("Запрос данных объявления | announ_id=%s", announ_id)
+
+    try:
+        data = await get_announ_page_db(announ_id)
+        return data
+
+    except Exception as e:
+        err = html.escape(str(e))
+        logger.error("Ошибка получения данных объявления | announ_id=%s | error=%s", announ_id, err)
+        return {"error": err}

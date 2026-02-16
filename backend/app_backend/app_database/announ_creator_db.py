640227ce4dbf5868ffa872791f5632913688bb90
ArturErnandes <al211205@yandex.ru>
2026-02-16 12:05:59 +0300

    Добавлен импорт сессии из db_engine

--- backend/app_backend/app_database/announ_creator_db.py
+++ backend/app_backend/app_database/announ_creator_db.py
@@ -1,22 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
 
-from ..app_config import DbConfig
+from .db_engine import new_session
 from ..app_classes import AnnounCreateSchema
 from ..logger import get_logger
 
 
 logger = get_logger(__name__)
-
-engine = create_async_engine(
-    f'postgresql+asyncpg://{DbConfig.admin}:{DbConfig.password}@{DbConfig.host}:{DbConfig.port}/{DbConfig.db_name}')
-
-new_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_session():
-    async with new_session() as session:
-        yield session
 
 
 async def post_announ_imgs_db(session, announ_id: int, imgs: list[str]):

fc151773bc9dee2bb525ab5ddcee01716899777a
ArturErnandes <al211205@yandex.ru>
2026-02-16 11:46:52 +0300

    Добавлен импорт класса AnnounCreateSchema

--- backend/app_backend/pages/announ_creator.py
+++ backend/app_backend/pages/announ_creator.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import html
 
+from ..app_classes import AnnounCreateSchema
 from ..app_database.announ_creator_db import post_announ_db
 from ..logger import get_logger
 
@@ -11,7 +12,7 @@ announ_creator = APIRouter()
 
 
 @announ_creator.post("/create_announ", tags=["Страница создания объявления"], summary="Создание объявления")
-async def create_announ(announ_data :dict):
+async def create_announ(announ_data: AnnounCreateSchema):
     logger.info("Запрос создания объявления")
 
     try:

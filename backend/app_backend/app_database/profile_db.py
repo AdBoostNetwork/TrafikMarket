93d03d9098bbecf0573aecf3554fb82b8b44b54a
ArturErnandes <al211205@yandex.ru>
2026-02-16 12:06:08 +0300

    Добавлен импорт сессии из db_engine

--- backend/app_backend/app_database/profile_db.py
+++ backend/app_backend/app_database/profile_db.py
@@ -1,24 +1,11 @@
-#Файл с функциями БД
-
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
 
-from ..app_config import DbConfig
+from .db_engine import new_session
 from ..logger import get_logger
 
 
 logger = get_logger(__name__)
 
-engine = create_async_engine(
-    f'postgresql+asyncpg://{DbConfig.admin}:{DbConfig.password}@{DbConfig.host}:{DbConfig.port}/{DbConfig.db_name}',
-)
-
-new_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_session():
-    async with new_session() as session:
-        yield session
 
 async def get_user_transactions(session, user_id: int):
     logger.info("Запрос транзакций пользователя | user_id: %s", user_id)

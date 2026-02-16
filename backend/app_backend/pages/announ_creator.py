4836e96e5228dd68e5f7edfec08e4391d90faab5
ArturErnandes <al211205@yandex.ru>
2026-02-16 11:09:03 +0300

    Added database function call to create announ entry

--- backend/app_backend/pages/announ_creator.py
+++ backend/app_backend/pages/announ_creator.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import html
 
+from ..app_database.announ_creator_db import post_announ_db
 from ..logger import get_logger
 
 
@@ -9,6 +10,15 @@ logger = get_logger(__name__)
 announ_creator = APIRouter()
 
 
-@announ_creator.post("/create_announ")
-async def create_announ():
-    ...
+@announ_creator.post("/create_announ", tags=["Страница создания объявления"], summary="Создание объявления")
+async def create_announ(announ_data :dict):
+    logger.info("Запрос создания объявления")
+
+    try:
+        result = await post_announ_db(announ_data)
+        return result
+
+    except Exception as e:
+        err = html.escape(str(e))
+        logger.error("Ошибка создания объявления | error=%s", err)
+        return {"error": err}

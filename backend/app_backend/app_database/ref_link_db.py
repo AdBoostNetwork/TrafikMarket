a2e09e2672b785998f68cb5ef450378f68166713
ArturErnandes <al211205@yandex.ru>
2026-02-16 12:06:11 +0300

    Добавлен импорт сессии из db_engine

--- backend/app_backend/app_database/ref_link_db.py
+++ backend/app_backend/app_database/ref_link_db.py
@@ -1,21 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
 
-from ..app_config import DbConfig
+from .db_engine import new_session
 from ..logger import get_logger
 
 
 logger = get_logger(__name__)
-
-engine = create_async_engine(
-    f'postgresql+asyncpg://{DbConfig.admin}:{DbConfig.password}@{DbConfig.host}:{DbConfig.port}/{DbConfig.db_name}')
-
-new_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_session():
-    async with new_session() as session:
-        yield session
 
 
 async def get_ref_link_db(user_id: int):

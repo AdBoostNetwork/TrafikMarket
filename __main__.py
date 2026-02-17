305ec4841935917e32924eb1386ae735aa9a85c6
ArturErnandes <al211205@yandex.ru>
2026-02-17 23:06:27 +0300

    запуск

--- __main__.py
+++ __main__.py
@@ -1,7 +1,19 @@
-#from  bots.support_bot.bot import run as sup_bot_run
-from bots.main_bot.bot import run as main_bot_run
-#from backend.app_backend.app import app_run
+from multiprocessing import Process
 
-#sup_bot_run()
-main_bot_run()
-#app_run()
+from bots.support_bot.bot import run as sup_bot_run
+from bots.main_bot.bot import run as main_bot_run
+from backend.app_backend.app import app_run
+
+
+if __name__ == "__main__":
+    procs = [
+        Process(target=sup_bot_run, name="support-bot"),
+        Process(target=main_bot_run, name="main-bot"),
+        Process(target=app_run, name="fastapi"),
+    ]
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()

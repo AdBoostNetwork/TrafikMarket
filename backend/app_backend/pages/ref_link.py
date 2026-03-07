7c7fcdd025c80e0ca548081942bebc0f49492b4a
ArturErnandes <al211205@yandex.ru>
2026-03-07 17:59:22 +0300

    Изменен вызов ручки

--- backend/app_backend/pages/ref_link.py
+++ backend/app_backend/pages/ref_link.py
@@ -12,11 +12,7 @@ ref_link_page = APIRouter()
 @ref_link_page.get("/ref_link", tags=["Реферальная ссылка"], summary="Получение реферальной ссылки пользователя")
 async def get_ref_link(user_id: int):
     try:
-        ref_link = await get_ref_link_db(user_id)
-        logger.info("Ссылка пользователя %s успешно получена | %s", user_id, ref_link)
-
-        return {"success": ref_link}
-
+        return await get_ref_link_db(user_id)
     except Exception as e:
         short_error = html.escape(str(e))
         logger.error("Ошибка при получении реферальной ссылки пользователя %s | %s", user_id, short_error)

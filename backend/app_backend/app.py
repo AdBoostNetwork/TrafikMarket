85ba4cefc2e9a95692da376674466e8f30f23bb1
ArturErnandes <al211205@yandex.ru>
2026-03-10 10:15:08 +0300

    Исправлен импорт страницы объявления

--- backend/app_backend/app.py
+++ backend/app_backend/app.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from.endpoints import endpoints
-from .pages.opened_announ import opened_announ_page
 from .pages.announs import announs_page
 from .pages.announ_creator import announ_creator
 from .app_config import AppConfig
@@ -33,6 +32,5 @@ app.add_middleware(
 )
 
 app.include_router(endpoints)
-app.include_router(opened_announ_page)
 app.include_router(announs_page)
 app.include_router(announ_creator)

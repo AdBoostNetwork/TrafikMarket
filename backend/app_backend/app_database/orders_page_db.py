468e0c16b810434661429ec90e38bfaaff50fa96
ArturErnandes <al211205@yandex.ru>
2026-04-05 01:00:19 +0300

    Функция получения завершенных заказов

--- backend/app_backend/app_database/orders_page_db.py
+++ backend/app_backend/app_database/orders_page_db.py
@@ -62,5 +62,9 @@ async def get_active_orders_db(user_id: int):
     logger.info(f"Получение активных заказов пользователя | user_id: %s", user_id)
 
 
+async def get_closed_orders_db(user_id: int):
+    logger.info(f"Получение завершенных заказов пользователя | user_id: %s", user_id)
+
+
 async def edit_announ_db(announ_id: int, data: AnnounCreateSchema):
     ...

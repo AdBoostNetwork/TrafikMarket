a2eaa9bf381aa1a6b909c7e4265495788c43880e
Artur PC <al211205@yandex.ru>
2026-04-05 12:47:03 +0300

    get_user_announs_db

--- backend/app_backend/app_database/orders_page_db.py
+++ backend/app_backend/app_database/orders_page_db.py
@@ -1,33 +1,11 @@
 from sqlalchemy import text
 
 from backend.db_engine import new_session
-from ..app_classes import ClosedAnnoun, AnnounCreateSchema
-from .helpers_db import get_seller_info_db, get_user_announs_data_db
+from ..app_classes import AnnounCreateSchema
 from ..logger import get_logger
 
 
 logger = get_logger(__name__)
-
-
-async def get_my_announs_db(user_id: int):
-    logger.info("Получение объявлений пользователя | user_id: %s", user_id)
-
-    async with new_session() as session:
-        seller = await get_seller_info_db(session, user_id)
-        rows = await get_user_announs_data_db(session, user_id)
-        announs_list = []
-
-        for row in rows:
-            announ = ClosedAnnoun(
-                announ_id=int(row["announ_id"]),
-                seller=seller,
-                title=row["title"],
-                price=float(row["price"]),
-                description=row["description"],
-            )
-            announs_list.append(announ)
-
-    return announs_list
 
 
 async def get_active_orders_db(user_id: int):

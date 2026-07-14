5fae30c948573984745f001d3caf888a3c2382b8
ArturErnandes <al211205@yandex.ru>
2026-07-14 16:11:33 +0300

    ai-assistant

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
 from multiprocessing import Process
 
-from bots.support_bot.bot import run as sup_bot_run
-from bots.main_bot.bot import run as main_bot_run
+#from bots.support_bot.bot import run as sup_bot_run
+#from bots.main_bot.bot import run as main_bot_run
 from backend.app_backend.app import app_run
 
 
 if __name__ == "__main__":
     procs = [
-        Process(target=sup_bot_run, name="support-bot"),
-        Process(target=main_bot_run, name="main-bot"),
+        #Process(target=sup_bot_run, name="support-bot"),
+        #Process(target=main_bot_run, name="main-bot"),
         Process(target=app_run, name="fastapi"),
     ]
 
